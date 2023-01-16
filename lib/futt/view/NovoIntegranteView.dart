9b7cf5c2e2f66ce8a61a2cb38b42c94058ee74bb
Robson Brito <robson.rmb@gmail.com>
2023-01-16 10:23:38 +0000

    Limpeza de código desnecessário.

--- lib/futt/view/NovoIntegranteView.dart
+++ lib/futt/view/NovoIntegranteView.dart
@@ -25,54 +25,9 @@ class _NovoIntegranteViewState extends State<NovoIntegranteView> {
       _msg = "Informe o email.";
 
     }else {
-      try {
-        IntegranteModel integranteModel = IntegranteModel.Novo(id, _controllerEmail.text);
-        //TorneioService torneioService = TorneioService();
-        //torneioService.adicionaIntegrante(integranteModel, ConstantesConfig.SERVICO_FIXO);
-
-        http.Response response = await http.post(
-            "https://jsonplaceholder.typicode.com/posts",
-            headers: <String, String>{
-              'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: json.encode({
-              "userId": 200,
-              "id": null,
-              "title": "Título",
-              "body": "Corpo da mensagem"
-            })
-        );
-        if (response.statusCode >= 200 && response.statusCode < 300) {
-          if (_mensagem == "Integrante inserido com sucesso!!!") {
-            _mensagem = "Novo integrante inserido com sucesso!!!";
-          }else{
-            _mensagem = "Integrante inserido com sucesso!!!";
-          }
-          _msg = _mensagem;
-          setState(() {
-            _controllerEmail.text = "";
-            _idTorneio = id;
-          });
-          //Navigator.pop(context);
-
-        } else {
-          _msg = "Falha durante o processamento!!!";
-        }
-
-      } on Exception catch (exception) {
-        print(exception.toString());
-        setState(() {
-          _msg = "Falha durante o processamento!!!";
-        });
-      } catch (error) {
-        setState(() {
-          _msg = "Falha durante o processamento!!!";
-        });
-      }
+      IntegranteModel integranteModel = IntegranteModel.Novo(
+          id, _controllerEmail.text);
     }
-    setState(() {
-      _mensagem = _msg;
-    });
   }
 
   @override
